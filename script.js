08e2887b2c572c9c03c98a33485349197259586e
nestorpirc-prog <nestorpirc@gmail.com>
2026-03-08 21:34:01 +0200

    Refactor downloadGame function and add score tracking

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
+let score = 0
+
+const planet = document.getElementById("planet")
+const scoreText = document.getElementById("score")
+
+planet.onclick = function(){
+
+score++
+
+scoreText.innerText = score
+
+}
+
 function downloadGame(){
 
-alert("Скоро тут буде скачування SpaceClicker 🚀");
+alert("Download will be available soon 🚀")
 
 }
