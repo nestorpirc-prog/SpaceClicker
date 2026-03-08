615cb56438bda2bf1805f19bdcf5faaadc9c8398
nestorpirc-prog <nestorpirc@gmail.com>
2026-03-08 21:42:34 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let score = 0
 
 const planet = document.getElementById("planet")
+
 const scoreText = document.getElementById("score")
 
 planet.onclick = function(){
@@ -11,8 +12,20 @@ scoreText.innerText = score
 
 }
 
+/* EASTER EGG */
+
+document.addEventListener("keydown", function(e){
+
+if(e.key === "s"){
+
+alert("🌌 Secret discovered! Welcome, space explorer!")
+
+}
+
+})
+
 function downloadGame(){
 
-alert("Download will be available soon 🚀")
+alert("🚀 Download coming soon!")
 
 }
